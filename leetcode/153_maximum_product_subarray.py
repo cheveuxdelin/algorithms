9fb89d971d84f5467ba15a788ec120ee062803b4
cheveuxdelin <victorba2120@gmail.com>
2022-10-12 17:55:28 -0500

    added solution files

---
+++ leetcode/153_maximum_product_subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProduct(self, nums: list[int]) -> int:
+        rtn = max(nums)
+        current_min = current_max = 1
+        for n in nums:
+            tmp = current_max * n
+            current_max = max(n * current_max, n * current_min, n)
+            current_min = min(tmp, n * current_min, n)
+            rtn = max(rtn, current_max)
+        return rtn
